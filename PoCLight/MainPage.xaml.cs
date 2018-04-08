15a41628b983f39b051f4aa838734ce759b2b5d1
Berkay <berkaysit@gmail.com>
2018-04-08 19:34:44 +0300

    LocalTime düzgün.

--- PoCLight/MainPage.xaml.cs
+++ PoCLight/MainPage.xaml.cs
@@ -31,6 +31,7 @@ namespace PoCLight
         public double Humidity { get; set; }
         public double Pressure { get; set; }
         public string DateTime { get; set; }
+        public double UVIndex { get; set; }
     }
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -41,6 +42,8 @@ namespace PoCLight
         static APDS9960Sensor apds;
         // BME280Sensor
         static BME280Sensor bme;
+        // VEML6075 Sensor
+        static VEML6075Sensor veml;
 
         static int count = 0;
 
@@ -55,15 +58,15 @@ namespace PoCLight
         private async void Initialize()
         {
             // Initialize and configure sensor: APDS9960
-            await InitializeAPDS9960();
+            await InitializeSensors();
             // Initialize and configure sensor: BME280Sensor
             //await InitializeBME280Sensor();
 
             // Configure timer to 2000ms delayed start and 60000ms interval
-            sensorTimer = new Timer(new TimerCallback(SensorTimerTick), null, 2000, 30000);
+            sensorTimer = new Timer(new TimerCallback(SensorTimerTick), null, 2000, 60000);
         }
 
-        private async Task InitializeAPDS9960()
+        private async Task InitializeSensors()
         {
             // Create sensor instance: APDS9960
             // Ambient & RGB light: enabled, proximity: disabled, gesture: disabled
@@ -74,6 +77,18 @@ namespace PoCLight
 
             // Delay 1ms
             await Task.Delay(1);
+
+            // Create sensor instance: VEML6075Sensor
+            veml = new VEML6075Sensor();
+
+            // Advanced sensor configuration: VEML6075Sensor
+            await veml.Config(
+                VEML6075Sensor.IntegrationTime.IT_800ms,
+                VEML6075Sensor.DynamicSetting.High,
+                VEML6075Sensor.Trigger.NoActiveForceTrigger,
+                VEML6075Sensor.ActiveForceMode.NormalMode,
+                VEML6075Sensor.PowerMode.PowerOn
+                );
         }
 
         private static void SensorTimerTick(object state)
@@ -85,6 +100,7 @@ namespace PoCLight
             double derece = bme.ReadTemperature();
             double nem = bme.ReadHumidity();
             double basinc = bme.ReadPressure();
+            double UVIndex = veml.Calculate_Average_UV_Index();
 
             // Create telemetry instance to store sensor data
             Telemetry telemetry = new Telemetry();
@@ -93,12 +109,18 @@ namespace PoCLight
             telemetry.Temperature = Math.Round(derece, 2);
             telemetry.Humidity = Math.Round(nem, 2);
             telemetry.Pressure = Math.Round(basinc, 2);
+            telemetry.UVIndex = Math.Round(UVIndex, 2);
 
-            // Set Measure Time
+            // Set Measurement Time
             DateTime localDate = DateTime.Now;
 
-            string utcFormat = localDate.ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'");
-            telemetry.DateTime = utcFormat;
+            //string utcFormat = localDate.ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'");
+            telemetry.DateTime = TimeZoneInfo.ConvertTime(localDate.ToUniversalTime(), TimeZoneInfo.Local).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'");
+
+            // Ex: Get GTB Standard Time zone - (GMT+02:00) Athens, Istanbul, Minsk
+            //TimeZoneInfo SystemTimeZoneId = TimeZoneInfo.FindSystemTimeZoneById("GTB Standard Time");
+            //DateTime LocalTime = TimeZoneInfo.ConvertTime(localDate, TimeZoneInfo.Local, SystemTimeZoneId);
+            //Debug.WriteLine("LocalTime: " + LocalTime);
 
             // Write sensor data to output / immediate window
             Debug.WriteLine("Date Time: " + telemetry.DateTime);
@@ -107,9 +129,17 @@ namespace PoCLight
             Debug.WriteLine("Humidity: " + telemetry.Humidity.ToString());
             Debug.WriteLine("Pressure: " + telemetry.Pressure.ToString());
 
+            Debug.WriteLine("- - - - - - - - - - - - -");
+
+            // Write sensor data to output / immediate window
+            Debug.WriteLine("UVA........: " + veml.Read_RAW_UVA().ToString());
+            Debug.WriteLine("UVB........: " + veml.Read_RAW_UVB().ToString());
+            Debug.WriteLine("UVA Index..: " + veml.Calculate_UV_Index_A().ToString());
+            Debug.WriteLine("UVB Index..: " + veml.Calculate_UV_Index_B().ToString());
+            Debug.WriteLine("UV Index...: " + telemetry.UVIndex.ToString());
             count = count + 1;
             Debug.WriteLine("Sıra: " + count);
-            Debug.WriteLine("--------------");
+            Debug.WriteLine("--------------------------");
 
             // Convert telemetry JSON to string
             string telemetryJSON = JsonConvert.SerializeObject(telemetry);
@@ -132,6 +162,11 @@ namespace PoCLight
             var message = new Message(Encoding.ASCII.GetBytes(msg));
 
             await deviceClient.SendEventAsync(message);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
